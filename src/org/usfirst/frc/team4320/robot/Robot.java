3e0d317f2cd8d54f04233eb12b15523ad38110c2
orike122 <oriko122@gmail.com>
2017-07-30 22:13:53 +0300

    assign drive command to robot

--- src/org/usfirst/frc/team4320/robot/Robot.java
+++ src/org/usfirst/frc/team4320/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import org.usfirst.frc.team4320.robot.commands.Drive;
 import org.usfirst.frc.team4320.robot.commands.ExampleCommand;
 import org.usfirst.frc.team4320.robot.subsystems.DriveSubsystem;
 import org.usfirst.frc.team4320.robot.subsystems.ExampleSubsystem;
@@ -39,7 +40,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
+		chooser.addDefault("Default Auto", new Drive());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
 	}
