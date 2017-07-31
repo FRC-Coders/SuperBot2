974efae39326ee2279b3707b942a417567a332fd
orike122 <oriko122@gmail.com>
2017-07-31 15:58:17 +0300

    added eject gear

---
+++ src/org/usfirst/frc/team4320/robot/commands/GrabGear.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team4320.robot.commands;
+
+import org.usfirst.frc.team4320.robot.Robot;
+import org.usfirst.frc.team4320.robot.subsystems.DriveSubsystem;
+import org.usfirst.frc.team4320.robot.subsystems.GearJaws.GrabberState;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class GrabGear extends Command {
+	public GrabGear(){
+		requires(Robot.gearJaws);
+	}
+	@Override
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Robot.gearJaws.setGrabber(GrabberState.insert);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		Robot.gearJaws.setGrabber(GrabberState.eject);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		end();
+	}
+
+}
